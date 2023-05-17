cfe51c688a1c87b71ada4c895403dbc9b9fb10f3
Tharun R S <tharunjananirs@gmail.com>
2023-05-17 08:08:21 +0530

    dietpro temp css

--- dietpro.php
+++ dietpro.php
@@ -42,22 +42,152 @@ if (isset($_POST['submit'])) {
 
 
 	<style>
-		/* Hide the default radio buttons */
-		input[type="radio"] {
-			display: none;
+		/* HIDE RADIO */
+		[type=radio] {
+			position: absolute;
+			opacity: 0;
+			width: 0;
+			height: 0;
 		}
 
-		/* Style the label as an image button */
-		label.image-button {
-			display: inline-block;
+		/* IMAGE STYLES */
+		[type=radio]+img {
+			width: 100%;
+			height: 50vh;
 			cursor: pointer;
 		}
 
-		/* Style the label when selected */
-		input[type="radio"]:checked+label.image-button {
-			border: 2px solid blue;
-			/* Example border style */
+		/* CHECKED STYLES */
+		[type=radio]:checked+img {
+			outline: 2px solid #fff;
+			border: 2px solid #fff;
+			border-radius: 25px;
 		}
+
+
+		#question1,
+		#question2,
+		#question3 {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: center;
+			width: 100%;
+			height: 20%;
+		}
+
+		.row {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: center;
+
+		}
+
+		.qcontainer {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: center;
+			margin: auto;
+			width: 45%;
+			height: auto;
+			position: relative;
+		}
+
+		form {
+			display: flex;
+			width: 100%;
+			height: auto;
+		}
+
+		label {
+			color: #fff;
+			text-align: center;
+		}
+
+
+		.buttonrst {
+
+			display: inline-block;
+			width: 100px;
+			outline: none;
+			cursor: pointer;
+			font-size: 14px;
+			line-height: 1;
+			border-radius: 500px;
+			transition-property: background-color, border-color, color, box-shadow, filter;
+			transition-duration: .3s;
+			border: 1px solid transparent;
+			letter-spacing: 2px;
+			text-transform: uppercase;
+			white-space: normal;
+			font-weight: 700;
+			text-align: center;
+			padding: 16px 14px 18px;
+			color: #e6e8eb;
+			box-shadow: inset 0 0 0 2px #616467;
+			background-color: transparent;
+			height: 48px;
+		}
+
+		.buttonrst:hover {
+			color: #616467;
+			background-color: #e6e8eb;
+		}
+
+		.range {
+			width: 400px;
+			height: 15px;
+			-webkit-appearance: none;
+			background: #111;
+			outline: none;
+			border-radius: 15px;
+			overflow: hidden;
+			box-shadow: inset 0 0 5px rgba(0, 0, 0, 1);
+		}
+
+		.range::-webkit-slider-thumb {
+			-webkit-appearance: none;
+			width: 15px;
+			height: 15px;
+			border-radius: 50%;
+			background: #fff;
+			cursor: pointer;
+			border: 4px solid #333;
+			box-shadow: -407px 0 0 400px #fff;
+		}
+
+		output {
+			color: #fff;
+			font-size: 2vw;
+		}
+
+
+		.outp {
+			padding-top: 2vh;
+			font-size: 2vw;
+			padding-bottom: 2vh;
+			color: #fff;
+			width: 100%;
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: center;
+			margin: auto;
+		}
+
+		input[type=number] {
+			width: 10%;
+			padding: 12px 20px;
+			margin: 8px 0;
+			box-sizing: border-box;
+			border: 2px solid #ccc;
+			border-radius: 4px;
+			background-color: #f8f8f8;
+			font-size: 16px;
+			color: #555;
+		}	
 	</style>
 
 </head>
@@ -90,42 +220,54 @@ if (isset($_POST['submit'])) {
 	<section class="pricing-section spad">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-12 col-sm-6">
-					<form id="questions-form" method="post">
-						<div id="question1">
-							<label>Are you obese?</label>
-							<input type="radio" id="obs-yes" name="obese" value="yes">
-							<label for="option1" class="image-button">
-								<img src="img/questions/obese-yes.png" alt="YES">
+				<form id="questions-form" method="post">
+					<div id="question1">
+						<label style="width: 100%; font-size: 4vw;">Are you obese?</label>
+						<div class="qcontainer">
+							<label for="obs-yes" class="image-button">
+								<input type="radio" id="obs-yes" name="obese" value="yes">
+								<img src="img/questions/obese-yes.png" class="imgq" alt="YES">
 								YES
 							</label>
 
-							<input type="radio" id="obs-no" name="obese" value="no">
-							<label for="option2" class="image-button">
-								<img src="img/questions/obese-no.png" alt="NO">
+						</div>
+						<div class="qcontainer">
+							<label for="obs-no" class="image-button">
+								<input type="radio" id="obs-no" name="obese" value="no">
+								<img src="img/questions/obese-no.png" class="imgq" alt="NO">
 								NO
 							</label>
-							<input type="button" value="Next" onclick="viewquestion2()">
+						</div>
+						<input type="button" class="buttonrst" value="Next" onclick="viewquestion2()" style="width: 30%">
+					</div>
+
+					<div id="question2" style="display: none;">
+						<label style="width: 100%; font-size: 4vw; padding-bottom:2vh" for="age">Select your age:</label>
+						<div class="qcontainer" style="width: 100%;">
+							<input type="range" class="range" id="age" name="age" min="0" max="100" step="1" value="18" style="width: 30%;">
+							<div class="outp">
+								<output for="age" id="ageOutput">18</output> years old
+							</div>
 						</div>
 
-						<div id="question2" style="display: none;">
-							<label for="age">Select your age:</label>
-							<input type="range" id="age" name="age" min="0" max="100" step="1" value="18">
-							<output for="age" id="ageOutput">18</output> years old
+						<input type="button" class="buttonrst" value="Previous" onclick="prev1()" style="width: 30%">
+						<input type="button" class="buttonrst" value="Next" onclick="viewquestion3()" style="width: 30%">
+					</div>
 
-							<input type="button" value="Next" onclick="viewquestion3()">
-						</div>
-
-						<div id="question3" style="display: none;">
-							<label for="height">Height (cm):</label>
+					<div id="question3" style="display: none;">
+						<label style="width: 100%; font-size: 4vw; padding-bottom:2vh" for="height">Height (cm):</label>
+						<div class="qcontainer" style="width: 100%;">
 							<input type="number" id="height" name="height" min="0" max="100" step="1">
-
-							<label for="weight">Weight (kg):</label>
-							<input type="number" id="weight" name="weight" min="0" max="500" step="0.1">
-
-							<input type="submit" name="submit" value="Submit">
 						</div>
-					</form>
+						<label style="width: 100%; font-size: 4vw; padding-bottom:2vh" for="weight">Weight (kg):</label>
+						<div class="qcontainer" style="width: 100%; padding-bottom:5vh">
+							<input type="number" id="weight" name="weight" min="0" max="500" step="0.1">
+						</div>
+
+						<input type="button" class="buttonrst" value="Previous" onclick="prev2()" style="width: 30%">
+						<input type="submit" class="buttonrst" name="submit" value="Submit" style="width: 30%">
+					</div>
+				</form>
 
 
 
@@ -134,44 +276,49 @@ if (isset($_POST['submit'])) {
 
 
 
-					<script>
-						var ageSlider = document.getElementById("age");
-						var ageOutput = document.getElementById("ageOutput");
+				<script>
+					var ageSlider = document.getElementById("age");
+					var ageOutput = document.getElementById("ageOutput");
 
-						// Update the age output when the slider value changes
-						ageSlider.addEventListener("input", function() {
-							ageOutput.textContent = ageSlider.value;
-						});
+					// Update the age output when the slider value changes
+					ageSlider.addEventListener("input", function() {
+						ageOutput.textContent = ageSlider.value;
+					});
 
-						function viewquestion2() {
-							var ques1 = document.getElementById('question1');
-							var ques2 = document.getElementById('question2');
+					function viewquestion2() {
+						var ques1 = document.getElementById('question1');
+						var ques2 = document.getElementById('question2');
 
-							ques1.style.display = "none";
-							ques2.style.display = "block";
-						}
+						ques1.style.display = "none";
+						ques2.style.display = "flex";
+					}
 
-						function viewquestion3() {
-							var ques2 = document.getElementById('question2');
-							var ques3 = document.getElementById('question3');
+					function viewquestion3() {
+						var ques2 = document.getElementById('question2');
+						var ques3 = document.getElementById('question3');
 
-							ques2.style.display = "none";
-							ques3.style.display = "block";
-						}
-					</script>
+						ques2.style.display = "none";
+						ques3.style.display = "flex";
+					}
+
+					function prev1() {
+						var ques2 = document.getElementById('question2');
+						var ques1 = document.getElementById('question1');
+
+						ques2.style.display = "none";
+						ques1.style.display = "flex";
+					}
+
+					function prev2() {
+						var ques2 = document.getElementById('question2');
+						var ques3 = document.getElementById('question3');
+
+						ques3.style.display = "none";
+						ques2.style.display = "flex";
+					}
+				</script>
 
 
-
-
-
-
-
-
-
-
-
-
-				</div>
 			</div>
 		</div>
 	</section>
