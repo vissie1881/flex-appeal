37af791cf28d68e95f718f5b914058a46f149e6f
Tharun R S <tharunjananirs@gmail.com>
2023-05-28 19:36:05 +0530

    updated style of change passwd and profile

--- changepassword.php
+++ changepassword.php
@@ -3,12 +3,12 @@ session_start();
 error_reporting(0);
 require_once('include/config.php');
 if(strlen( $_SESSION["uid"])==0)
-    {   
+{   
 header('location:login.php');
 }
 else{
 // Code for change password	
-if(isset($_POST['submit']))
+	if(isset($_POST['submit']))
 	{
 $password=md5($_POST['password']);
 $newpassword=md5($_POST['newpassword']);
@@ -33,40 +33,105 @@ $error="Your current password is not valid.";
 }
 }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="zxx">
-<head>
-	<title>BulkBois | Changepassword</title>
-	<link rel="icon" type="image/x-icon" href="img/bell.png">
-	<meta charset="UTF-8">
-	<meta name="description" content="Ahana Yoga HTML Template">
-	<meta name="keywords" content="yoga, html">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="css/font-awesome.min.css"/>
-	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
-	<link rel="stylesheet" href="css/nice-select.css"/>
-	<link rel="stylesheet" href="css/magnific-popup.css"/>
-	<link rel="stylesheet" href="css/slicknav.min.css"/>
-	<link rel="stylesheet" href="css/animate.css"/>
 
-	<!-- Main Stylesheets -->
-	<link rel="stylesheet" href="css/style.css"/>
-<script type="text/javascript">
-function valid()
-{
-if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value)
-{
-alert("New Password and Confirm Password Field do not match  !!");
-document.chngpwd.confirmpassword.focus();
-return false;
-}
-return true;
-}
-</script>
-  <style>
-		.errorWrap {
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>BulkBois | Changepwd</title>
+	<link rel="icon" type="image/x-icon" href="img/bell.png">
+		<script type="text/javascript">
+		function valid()
+		{
+		if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value)
+		{
+		alert("New Password and Confirm Password Field do not match  !!");
+		document.chngpwd.confirmpassword.focus();
+		return false;
+		}
+		return true;
+		}
+		</script>
+		<style>
+			.header-top{
+				background-color: rgba(0,0,0,0);
+			}
+		</style>
+
+</head>
+
+<body>
+	<!-- Header Section -->
+	<!-- Header Section end -->
+
+	<section class="vh-100"
+		style = "background-image: url(img/changepwdbg.jpg) ;background-repeat: no-repeat; background-size: cover; height: 100%;">
+		<div class="mask" style="background-color: rgba(0, 0, 0, 0);">
+		<?php include 'include/header.php';?>
+		<br><br><br><br><br><br>
+		<div class="container py-5 h-50">
+			<div class="row d-flex justify-content-center align-items-center h-50">
+				<div class="col-12 col-md-8 col-lg-6 col-xl-5">
+					<div class="card bg-transparent  text-white" style="border-radius: 1rem;">
+						<div class="card-body p-5 text-center bg-light" style="border-radius: 1rem; height:25rem;">
+							<div class="mb-md-5 mt-md-1 pb-5">
+								<h2 class="fw-bold mb-2 text-uppercase" style="color: black" >Change password</h2>
+								<!-- <p class="text-white-50 mb-3">Enter your details below!</p> -->
+								<?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+								<br>
+								<form class="singup-form contact-form" method="post" onSubmit="return valid();">
+									<div class="row" style="padding-left:50px; padding-right:50px;">
+										<div class="col-md-12">
+											<input type="password" name="password" id="password" placeholder="Old Password" autocomplete="off">
+										</div>
+										<div class="col-md-12">
+											<input type="password" name="newpassword" id="newpassword" placeholder="New Password" autocomplete="off">
+										</div>
+										<div class="col-md-12">
+											<input type="password" name="confirmpassword" id="confirmpassword" placeholder="Confirm Password" autocomplete="off">
+										</div>
+									</div>
+									<input type="submit" id="submit" name="submit" value="Submit" class="site-btn sb-gradient" style="width:50px;">
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- <div class="back-to-top"><img src="img/icons/up-arrow.png" alt=""></div> -->
+
+	<!-- Search model end -->
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
+
+<style>
+.errorWrap {
     padding: 10px;
     margin: 0 0 20px 0;
     background: #fff;
@@ -82,220 +147,6 @@ return true;
     -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
 }
-		</style>
-</head>
-<body>
-	<!-- Page Preloder -->
-	
+</style>
 
-	<!-- Header Section -->
-	<?php include 'include/header.php';?>
-	<!-- Header Section end -->
-
-	                                                                              
-	<!-- Page top Section -->
-	<!-- <section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-7 m-auto text-white">
-					<h2 style="font-size: 75px;">change password</h2>
-				</div>
-			</div>
-		</div>
-	</section> -->
-
-	<!-- Slideshow start-->
-	<section id="slider-sect">
-		<section class="page-top-section set-bg" data-setbg="img/gallery/13.jpg" style="display:none" id="back-img-13">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 m-auto text-white">
-						<h2 class="main_text" styles="font-size: 50px;">Change Password</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="page-top-section set-bg" data-setbg="img/gallery/7.jpg" style="display:none" id="back-img-7">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 m-auto text-white">
-						<h2 class="main_text" styles="font-size: 50px;">Change Password</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="page-top-section set-bg" data-setbg="img/gallery/8.jpg" style="display:none" id="back-img-8">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 m-auto text-white">
-						<h2 class="main_text" styles="font-size: 50px;">Change Password</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="page-top-section set-bg" data-setbg="img/gallery/9.jpg" style="display:none" id="back-img-9">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 m-auto text-white">
-						<h2 class="main_text" styles="font-size: 50px">Change Password</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="page-top-section set-bg" data-setbg="img/gallery/10.jpg" style="display:none" id="back-img-10">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 m-auto text-white">
-						<h2 class="main_text" styles="font-size: 50px;">Change Password</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="page-top-section set-bg" data-setbg="img/gallery/11.jpg" style="display:none" id="back-img-11">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 m-auto text-white">
-						<h2 class="main_text" styles="font-size:50px; color:red;">Change Password</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section class="page-top-section set-bg" data-setbg="img/gallery/12.jpg" style="display:none" id="back-img-12">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 m-auto text-white">
-						<h2 class="main_text" styles="font-size: 50px;">Change Password</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-	</section>
-	<!-- Slideshow end -->
-
-
-
-	<!-- Pricing Section -->
-	<section class="pricing-section spad">
-		<div class="container">
-			
-			<div class="row">
-				<div class="col-lg-4 col-sm-6">
-					
-				</div>
-				<div class="col-lg-4 col-sm-6">
-					<div class="pricing-item entermediate" style="height:440px;">
-						<div class="pi-top" style="height:100px;">
-							<h4>change password</h4>
-						</div>
-						  <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
-				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
-						<br>
-						<form class="singup-form contact-form" method="post" onSubmit="return valid();">
-						<div class="row" style="padding-left:50px; padding-right:50px;">
-							<div class="col-md-12">
-								<input type="password" name="password" id="password" placeholder="Old Password" autocomplete="off">
-							</div>
-							<div class="col-md-12">
-								<input type="password" name="newpassword" id="newpassword" placeholder="New Password" autocomplete="off">
-							</div>
-							
-							<div class="col-md-12">
-								<input type="password" name="confirmpassword" id="confirmpassword" placeholder="Confirm Password" autocomplete="off">
-							</div>
-							
-						</div>
-						<br>
-					
-					<input type="submit" id="submit" name="submit" value="Submit" class="site-btn sb-gradient" style="width:50px;">
-</form>
-					</div>
-				</div>
-				<div class="col-lg-4 col-sm-6">
-					
-				</div>
-				
-			</div>
-		</div>
-	</section>
-	
-
-	<!-- Footer Section -->
-	<?php include 'include/footer.php'; ?>
-	<!-- Footer Section end -->
-
-	<div class="back-to-top"><img src="img/icons/up-arrow.png" alt=""></div>
-
-	<!-- Search model end -->
-
-	<!--====== Javascripts & Jquery ======-->
-	<script src="js/vendor/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.slicknav.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/jquery.nice-select.min.js"></script>
-	<script src="js/jquery-ui.min.js"></script>
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/main.js"></script>
-	
-
-	<script>
-		var slide1 = document.getElementById('back-img-13');
-		var slide2 = document.getElementById('back-img-7');
-		var slide3 = document.getElementById('back-img-8');
-		var slide4 = document.getElementById('back-img-9');
-		var slide5 = document.getElementById('back-img-10');
-		var slide6 = document.getElementById('back-img-11');
-		var slide7 = document.getElementById('back-img-12');
-		var i = 0;
-		var intervalTime = 3000; // Interval time in milliseconds (3 seconds)
-
-		function changeSlide() {
-			switch (i % 7) {
-				case 0:
-					slide7.style.display = "none";
-					slide1.style.display = "block";
-					break;
-				case 1:
-					slide1.style.display = "none";
-					slide2.style.display = "block";
-					break;
-				case 2:
-					slide2.style.display = "none";
-					slide3.style.display = "block";
-					break;
-				case 3:
-					slide3.style.display = "none";
-					slide4.style.display = "block";
-					break;
-				case 4:
-					slide4.style.display = "none";
-					slide5.style.display = "block";
-					break;
-				case 5:
-					slide5.style.display = "none";
-					slide6.style.display = "block";
-					break;
-				case 6:
-					slide6.style.display = "none";
-					slide7.style.display = "block";
-					break;
-			}
-
-			i++;
-			setTimeout(changeSlide, intervalTime);
-		}
-
-		// Start the slideshow
-		changeSlide();
-	</script>
-
-
-	</body>
-</html>
-<?php } ?>	
+<?php } ?>
--- profile.php
+++ profile.php
@@ -2,176 +2,157 @@
 session_start();
 error_reporting(0);
 require_once('include/config.php');
-if(strlen( $_SESSION["uid"])==0)
-    {   
-header('location:login.php');
-}
-else{
+if (strlen($_SESSION["uid"]) == 0) {
+	header('location:login.php');
+} else {
 
 
-if(isset($_POST['submit']))
-{
-$uid=$_SESSION['uid'];
-$fname=$_POST['fname'];
-$lname=$_POST['lname'];
-$email=$_POST['email'];
-$mobile=$_POST['mobile'];
-$city=$_POST['city'];
-$state=$_POST['state'];
-$address=$_POST['address'];
-$sql="update tbluser set fname=:fname,lname=:lname,mobile=:mobile,city=:city,state=:state,address=:Address where id=:uid";
-$query = $dbh->prepare($sql);
-$query->bindParam(':fname',$fname,PDO::PARAM_STR);
-$query->bindParam(':lname',$lname,PDO::PARAM_STR);
-$query->bindParam(':mobile',$mobile,PDO::PARAM_STR);
-$query->bindParam(':city',$city,PDO::PARAM_STR);
-$query->bindParam(':state',$state,PDO::PARAM_STR);
-$query->bindParam(':Address',$address,PDO::PARAM_STR);
-$query->bindParam(':uid',$uid,PDO::PARAM_STR);
-$query->execute();
-//$msg="<script>toastr.success('Mobile info updated Successfully', {timeOut: 5000})</script>";
-echo "<script>alert('Profile has been updated.');</script>";
-echo "<script> window.location.href =profile.php;</script>";
-
-}
+	if (isset($_POST['submit'])) {
+		$uid = $_SESSION['uid'];
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$email = $_POST['email'];
+		$mobile = $_POST['mobile'];
+		$city = $_POST['city'];
+		$state = $_POST['state'];
+		$address = $_POST['address'];
+		$sql = "update tbluser set fname=:fname,lname=:lname,mobile=:mobile,city=:city,state=:state,address=:Address where id=:uid";
+		$query = $dbh->prepare($sql);
+		$query->bindParam(':fname', $fname, PDO::PARAM_STR);
+		$query->bindParam(':lname', $lname, PDO::PARAM_STR);
+		$query->bindParam(':mobile', $mobile, PDO::PARAM_STR);
+		$query->bindParam(':city', $city, PDO::PARAM_STR);
+		$query->bindParam(':state', $state, PDO::PARAM_STR);
+		$query->bindParam(':Address', $address, PDO::PARAM_STR);
+		$query->bindParam(':uid', $uid, PDO::PARAM_STR);
+		$query->execute();
+		//$msg="<script>toastr.success('Mobile info updated Successfully', {timeOut: 5000})</script>";
+		echo "<script>alert('Profile has been updated.');</script>";
+		echo "<script> window.location.href =profile.php;</script>";
+	}
 
 
- ?>
-<!DOCTYPE html>
-<html lang="zxx">
-<head>
-	<title>Gym Management System | User Profile</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="css/font-awesome.min.css"/>
-	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
-	<link rel="stylesheet" href="css/nice-select.css"/>
-	<link rel="stylesheet" href="css/slicknav.min.css"/>
+?>
+	<!DOCTYPE html>
+	<html lang="zxx">
 
-	<!-- Main Stylesheets -->
-	<link rel="stylesheet" href="css/style.css"/>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Main CSS-->
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- Font-icon css-->
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<title>BulkBois | Profile</title>
+		<link rel="icon" type="image/x-icon" href="img/bell.png">
+		<style>
+			.header-top{
+				background-color: rgba(0,0,0,0);
+			}
+		</style>
+	</head>
 
-</head>
-<body>
-	<!-- Page Preloder -->
-	
+	<body>
+		<section class="vh-100" style="background-image: url(img/changepwdbg.jpg) ;background-repeat: no-repeat; background-size: cover; height:100%">
+			<div class="mask" style="background-color: rgba(0, 0, 0, 0);">
+				<?php include 'include/header.php'; ?>
+				<br><br><br><br><br><br>
+				<div class="container py-5 h-50">
+					<div class="row d-flex justify-content-center align-items-center h-50">
+						<div class="col-12 col-md-8 col-lg-6 col-xl-5">
+							<div class="card bg-transparent  text-black" style="border-radius: 1rem;">
+								<div class="card-body p-5 text-center bg-light" style="border-radius: 1rem; height:30rem;">
+									<div class="mb-md-5 mt-md-1 pb-5">
+										<h2 class="fw-bold mb-2 text-uppercase" style="color: black">Profile</h2>
+										<!-- <p class="text-black-50 mb-3">Enter your details below!</p> -->
+										<form class="singup-form contact-form" method="post">
+											<div class="row">
+												<?php
+												$uid = $_SESSION['uid'];
+												$sql = "SELECT id, fname, lname, email, mobile, password, address,state,city, create_date from tbluser where id=:uid ";
+												$query = $dbh->prepare($sql);
+												$query->bindParam(':uid', $uid, PDO::PARAM_STR);
+												$query->execute();
+												$results = $query->fetchAll(PDO::FETCH_OBJ);
+												$cnt = 1;
+												if ($query->rowCount() > 0) {
+													foreach ($results as $result) {				?>
+														<div class="col-md-6">
+															<input type="text" name="fname" id="fname" placeholder="First Name" autocomplete="off" value="<?php echo $result->fname; ?>">
+														</div>
+														<div class="col-md-6">
+															<input type="text" name="lname" id="lname" placeholder="Last Name" autocomplete="off" value="<?php echo $result->lname; ?>">
+														</div>
+														<div class="col-md-6">
+															<input type="text" name="email" id="email" placeholder="Your Email" autocomplete="off" value="<?php echo $result->email; ?>" readonly>
+														</div>
+														<div class="col-md-6">
+															<input type="text" name="mobile" id="mobile" placeholder="Mobile Number" autocomplete="off" value="<?php echo $result->mobile; ?>">
+														</div>
+														<div class="col-md-6">
+															<input type="text" name="state" id="state" placeholder="State" autocomplete="off" value="<?php echo $result->state; ?>">
+														</div>
+														<div class="col-md-6">
+															<input type="text" name="city" id="city" placeholder="City" autocomplete="off" value="<?php echo $result->city; ?>">
+														</div>
 
-	<!-- Header Section -->
-	<?php include 'include/header.php';?>
-	<!-- Header Section end -->
-
-	
-	                                                                              
-	<!-- Page top Section -->
-	<section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-7 m-auto text-white">
-					<h2>Profile</h2>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- Page top Section end -->
-
-	<!-- Contact Section -->
-	<section class="contact-page-section spad overflow-hidden">
-		<div class="container">
-			
-			<div class="row">
-				<div class="col-lg-2">
-				</div>
-				<div class="col-lg-8">
-					<form class="singup-form contact-form" method="post">
-						<div class="row">
-							<?php 
-							$uid=$_SESSION['uid'];
-							$sql ="SELECT id, fname, lname, email, mobile, password, address,state,city, create_date from tbluser where id=:uid ";
-							$query= $dbh -> prepare($sql);
-							$query->bindParam(':uid',$uid, PDO::PARAM_STR);
-							$query-> execute();
-							$results = $query -> fetchAll(PDO::FETCH_OBJ);
-							$cnt=1;
-							if($query->rowCount() > 0)
-							{
-							foreach($results as $result)
-							{				?>	
-							<div class="col-md-6">
-								<input type="text" name="fname" id="fname" placeholder="First Name" autocomplete="off" value="<?php echo $result->fname;?>">
+														<div class="col-md-12">
+															<input type="text" name="address" id="address" placeholder="Address" autocomplete="off" value="<?php echo $result->address; ?>">
+														</div>
+														<div class="col-md-12">
+															<input type="submit" id="submit" name="submit" value="Update" class="site-btn sb-gradient">
+														</div>
+												<?php }
+												} ?>
+											</div>
+										</form>
+									</div>
+								</div>
 							</div>
-							<div class="col-md-6">
-								<input type="text" name="lname" id="lname" placeholder="Last Name" autocomplete="off" value="<?php echo $result->lname;?>">
-							</div>
-							<div class="col-md-6">
-								<input type="text" name="email" id="email" placeholder="Your Email" autocomplete="off" value="<?php echo $result->email;?>" readonly>
-							</div>
-							<div class="col-md-6">
-								<input type="text" name="mobile" id="mobile" placeholder="Mobile Number" autocomplete="off" value="<?php echo $result->mobile;?>">
-							</div>
-							<div class="col-md-6">
-								<input type="text" name="state" id="state" placeholder="State" autocomplete="off" value="<?php echo $result->state;?>">
-							</div>
-							<div class="col-md-6">
-								<input type="text" name="city" id="city" placeholder="City" autocomplete="off" value="<?php echo $result->city;?>">
-							</div>
-							
-							<div class="col-md-12">
-								<input type="text" name="address" id="address" placeholder="Address" autocomplete="off" value="<?php echo $result->address;?>">
-							</div>
-							<div class="col-md-12">
-						<input type="submit" id="submit" name="submit" value="Update" class="site-btn sb-gradient">
-								
-							</div>
-							<?php }} ?>
 						</div>
-					</form>
-				</div>
-				<div class="col-lg-2">
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-	<!-- Trainers Section end -->
-<?php include 'include/footer.php'; ?>
-	<!-- Footer Section end -->
-	
-	<div class="back-to-top"><img src="img/icons/up-arrow.png" alt=""></div>
+		</section>
 
-	<!-- Search model -->
-	
-	<!-- Search model end -->
 
-	<!--====== Javascripts & Jquery ======-->
-	<script src="js/vendor/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.slicknav.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/jquery.nice-select.min.js"></script>
-	<script src="js/jquery-ui.min.js"></script>
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/main.js"></script>
+		<!-- Search model -->
+
+		<!-- Search model end -->
+
+		<!--====== Javascripts & Jquery ======-->
+		<script src="js/vendor/jquery-3.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.slicknav.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.nice-select.min.js"></script>
+		<script src="js/jquery-ui.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/main.js"></script>
 
 	</body>
-</html>
- <?php } ?>
 
-  <style>
-.errorWrap {
-    padding: 10px;
-    margin: 0 0 20px 0;
-    background: #dd3d36;
-    color:#fff;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-}
-.succWrap{
-    padding: 10px;
-    margin: 0 0 20px 0;
-    background: #5cb85c;
-    color:#fff;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-}
-        </style>
+	</html>
+<?php } ?>
+
+<style>
+	.errorWrap {
+		padding: 10px;
+		margin: 0 0 20px 0;
+		background: #dd3d36;
+		color: #fff;
+		-webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+		box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+	}
+
+	.succWrap {
+		padding: 10px;
+		margin: 0 0 20px 0;
+		background: #5cb85c;
+		color: #fff;
+		-webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+		box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
+	}
+</style>
